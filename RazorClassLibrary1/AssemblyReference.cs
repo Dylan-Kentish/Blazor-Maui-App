37f90ccbe1163c7473834494dbdd4ff6ec815b48
Kent <dylankentish@live.co.uk>
2022-09-19 20:45:35 +0100

    Moved Pages into RazorClassLibrary1

---
+++ RazorClassLibrary1/AssemblyReference.cs
@@ -0,0 +1,16 @@
+﻿namespace RazorClassLibrary1
+{
+    /// <summary>
+    /// Temporary solution from https://stackoverflow.com/questions/11214138/get-assembly-reference-without-reflection-or-a-known-type
+    /// </summary>
+    public static class AssemblyReference
+    {
+        public static System.Reflection.Assembly Get
+        {
+            get
+            {
+                return typeof(AssemblyReference).Assembly;
+            }
+        }
+    }
+}
